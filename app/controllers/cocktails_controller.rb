0f86ed50326f8d7521291addbeb7e19d23417457
Coralie Delhommeau <coraliedelhommeau@gmail.com>
2020-11-20 16:29:15 +0100

    exo done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -9,7 +9,11 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
 
     # no need for app/views/restaurants/create.html.erb
     redirect_to cocktails_path
@@ -23,6 +27,6 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :picture_url)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
